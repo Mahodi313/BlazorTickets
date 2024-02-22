9140725e5be432c8d44924abca8e822ded0170d4
Gustav-Frising <frising96@gmail.com>
2024-02-22 18:37:37 +0100

    Controller-&-Services

---
+++ BlazorTickets/Services/ITicketsService.cs
@@ -0,0 +1,17 @@
+﻿using static BlazorTickets.Pages.TicketCenter;
+
+namespace BlazorTickets.Services
+{
+	public interface ITicketsService
+	{
+		public interface ITicketService
+		{
+			public HttpClient Client { get; set; }
+
+			Task<List<TicketModel>> GetTickets();
+			Task PostTicket(TicketModel ticketModel);
+			Task UpdateTicket(int ticketId, TicketModel ticketModel);
+			Task DeleteTicket(int ticketId);
+		}
+	}
+}
---
+++ Shared/Models/TagModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+	public class TagModel
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Name { get; set; } = null!; // Exempel: "CSharp", "JavaScript"
+		public List<TicketTag> TicketTags { get; set; } = new List<TicketTag>();
+	}
+}
